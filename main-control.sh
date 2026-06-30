d7ee9cc80fbb342e8f9e8d575d9c7afb08dd6272
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-06-30 22:55:07 +0900

    Remove logging from build scripts in main-control.sh

--- main-control.sh
+++ main-control.sh
@@ -9,14 +9,14 @@ Uri="http://ports.ubuntu.com/ubuntu-ports"
 	start_time=`date`
 
 	sudo rm -f log?
-	sudo ./build_kernel_env.sh orangepi-5-rk3588s_defconfig $Uri $suite kernel 2>&1|tee log0
-	sudo  ./mesa-build-env.sh arm64 $1 $Uri $suite 2>&1|tee log2
-	#sudo  ./meas-build-env.sh arm64 ubuntu $Uri $suite 2>&1|tee log2
-	sudo  ./rootfs-bootstrap.sh arm64 $Uri $suite 2>&1|tee log3
-	sudo  ./disk_image.sh arm64 orangepi-5 rk3588s-orangepi-5 2>&1|tee log4
+	sudo ./build_kernel_env.sh orangepi-5-rk3588s_defconfig $Uri $suite kernel
+	sudo  ./mesa-build-env.sh arm64 $1 $Uri $suite
+	#sudo  ./meas-build-env.sh arm64 ubuntu $Uri $suite
+	sudo  ./rootfs-bootstrap.sh arm64 $Uri $suite
+	sudo  ./disk_image.sh arm64 orangepi-5 rk3588s-orangepi-5
 	sudo mv overlay/u-boot-rockchip.bin overlay/orangepi-5-u-boot-rockchip.bin
-	sudo ./build_kernel_env.sh orangepi-5-plus-rk3588_defconfig $Uri $suite u-boot 2>&1|tee log5
-	sudo  ./disk_image.sh arm64 orangepi-5-plus rk3588-orangepi-5-plus 2>&1|tee log6
+	sudo ./build_kernel_env.sh orangepi-5-plus-rk3588_defconfig $Uri $suite u-boot
+	sudo  ./disk_image.sh arm64 orangepi-5-plus rk3588-orangepi-5-plus
 	sudo mv overlay/u-boot-rockchip.bin overlay/orangepi-5-PLUS-u-boot-rockchip.bin
 
 	echo "$start_time"
