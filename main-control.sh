3d77f84b89f765c986b0cd4f7c205fe6887fd8be
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-06-30 21:22:49 +0900

    Update URI for Ubuntu ports in main-control.sh

--- main-control.sh
+++ main-control.sh
@@ -2,15 +2,15 @@
 
 #suite=plucky
 suite=resolute
-Uri="http://ftp.udx.icscoe.jp/Linux/ubuntu-ports/"
-#Uri="http://ports.ubuntu.com/ubuntu-ports"
+#Uri="http://ftp.udx.icscoe.jp/Linux/ubuntu-ports/"
+Uri="http://ports.ubuntu.com/ubuntu-ports"
 
 
 	start_time=`date`
 
 	sudo rm -f log?
 	sudo ./build_kernel_env.sh orangepi-5-rk3588s_defconfig $Uri $suite kernel 2>&1|tee log0
-	sudo  ./meas-build-env.sh arm64 upstream $Uri $suite 2>&1|tee log2
+	sudo  ./mesa-build-env.sh arm64 upstream $Uri $suite 2>&1|tee log2
 	#sudo  ./meas-build-env.sh arm64 ubuntu $Uri $suite 2>&1|tee log2
 	sudo  ./rootfs-bootstrap.sh arm64 $Uri $suite 2>&1|tee log3
 	sudo  ./disk_image.sh arm64 orangepi-5 rk3588s-orangepi-5 2>&1|tee log4
